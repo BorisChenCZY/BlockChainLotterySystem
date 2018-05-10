9c76be91eab333e81499a9fab33bcb9839faf448
BorisChenCZY <boris.chen.czy@gmail.com>
2018-05-10 23:52:43 +0800

    unfinished vote

--- startvote/static/js/encryption.js
+++ startvote/static/js/encryption.js
@@ -1,26 +1,27 @@
 var sig;
 var rsaPrivateKey;
 
-function init(privateKey){
+function init(privateKey) {
     rsaPrivateKey = new RSAKey();
     rsaPrivateKey.readPrivateKeyFromPEMString(privateKey);
+    console.log("inited");
 }
 
-function sign(msg){
-	sig = new KJUR.crypto.Signature({"alg": "SHA1withRSA"});
-	sig.init(rsaPrivateKey);
-	return sig.signString(msg);
+function sign(msg) {
+    sig = new KJUR.crypto.Signature({"alg": "SHA256withRSAandMGF1"});
+    sig.init(rsaPrivateKey);
+    return sig.signString(msg);
 }
 
-function generate_pair(){
-	rsaKeypair = KEYUTIL.generateKeypair("RSA", 512);
-	return rsaKeypair;
+function generate_pair() {
+    rsaKeypair = KEYUTIL.generateKeypair("RSA", 512);
+    return rsaKeypair;
 }
 
-function toPCK1(key){
-	return KEYUTIL.getPEM(key, "PKCS1PRV");
+function toPCK1(key) {
+    return KEYUTIL.getPEM(key, "PKCS1PRV");
 }
 
-function toPKCS8(key){
-	return KEYUTIL.getPEM(key);
+function toPKCS8(key) {
+    return KEYUTIL.getPEM(key);
 }
