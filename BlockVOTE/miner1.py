f8829a9499381e85794e2bd5a40b26a5fd6843d0
BorisChenCZY <boris.chen.czy@gmail.com>
2018-06-14 15:28:25 +0800

    modified something

--- BlockVOTE/miner1.py
+++ BlockVOTE/miner1.py
@@ -3,7 +3,7 @@ from BlockVOTE.P2P.Node import BQUEUE
 from BlockVOTE.VoteInfo import VoteInfo
 import datetime
 if __name__ == '__main__':
-    addr = ("localhost", 9999)
+    addr = ("10.20.7.144", 6666)
     miner = Miner.instance(addr, "zsq", 0)
     # voteInfo = VoteInfo(datetime.datetime.now().timestamp(), b'target', b'pubkey', (2, [3]), b'sign')
     # miner.add_vote(bytes(voteInfo))
