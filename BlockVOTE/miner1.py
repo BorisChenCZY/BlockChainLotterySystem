f912ff6858da7466ce60a31d61e585d565844945
BorisChenCZY <boris.chen.czy@gmail.com>
2018-06-14 16:00:08 +0800

    zhangshiqiniubi

--- BlockVOTE/miner1.py
+++ BlockVOTE/miner1.py
@@ -4,7 +4,7 @@ from BlockVOTE.VoteInfo import VoteInfo
 import datetime
 if __name__ == '__main__':
     addr = ("10.20.7.144", 6666)
-    miner = Miner.instance(addr, "zsq", 0)
+    miner = Miner.instance(addr, "zsq1", 0)
     # voteInfo = VoteInfo(datetime.datetime.now().timestamp(), b'target', b'pubkey', (2, [3]), b'sign')
     # miner.add_vote(bytes(voteInfo))
     # block = miner.pack_block()
